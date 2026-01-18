db95ae3d0c9f52a51f8378221c01b7cf4fd63280
SatyaAdil <adilfaishal11@gmail.com>
2026-01-19 04:14:56 +0700

    fix routes

--- src/index.ts
+++ src/index.ts
@@ -19,11 +19,17 @@ app.use(
   cors({
     origin: [
       "http://localhost:3000",
+      "https://carbonscanemisi.vercel.app",
       "https://carbonscan-api.vercel.app",
     ],
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
   })
 )
+
+
+
 
 
 app.use(express.json())
