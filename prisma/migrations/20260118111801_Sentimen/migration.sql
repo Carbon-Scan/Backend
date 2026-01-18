bb636b33d4ffb2561e3dc91bb1cf1eeb2ac434b8
SatyaAdil <adilfaishal11@gmail.com>
2026-01-18 18:22:03 +0700

    fix: prisma migration reviews & anonymous review

--- prisma/migrations/20260118111801_Sentimen/migration.sql
+++ prisma/migrations/20260118111801_Sentimen/migration.sql
@@ -1,10 +1,10 @@
 -- CreateTable
 CREATE TABLE "reviews" (
     "id" TEXT NOT NULL,
-    "user_id" TEXT NOT NULL,
     "text" TEXT NOT NULL,
-    "sentiment" TEXT,
-    "confidence" DOUBLE PRECISION,
+    "sentiment" TEXT NOT NULL,
+    "confidence" DOUBLE PRECISION NOT NULL,
+    "email" TEXT,
     "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
 
     CONSTRAINT "reviews_pkey" PRIMARY KEY ("id")
